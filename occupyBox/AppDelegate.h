19b873fa7d3d82c989b479fbba0d5dc2f911fd50
biqinglin <biqinglin@biqinglin.local>
2017-04-19 15:31:26 +0800

    Initial Commit

---
+++ occupyBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  occupyBox
+//
+//  Created by biqinglin on 2017/4/19.
+//  Copyright © 2017年 biqinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
